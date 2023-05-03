56679e861375b4140f90f0b56a655ddc6c7888b9
florentianayuwono <e0851439@u.nus.edu>
2023-05-03 20:36:18 +0800

    Add useEffect basic

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("page rendered.");
+    if (value >= 1) {
+      document.title = `New Things!(${value})`;
+    }
+  });
+  console.log("Is it rendering?");
+  return (
+    <>
+      <h2>useEffect Basics</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Try me?
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
