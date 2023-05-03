50c6d7dcd8b58ca3f8b833c829cb76a670ab25d2
florentianayuwono <e0851439@u.nus.edu>
2023-05-03 19:45:15 +0800

    Add useState with object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Susanh",
+    age: 10,
+    message: "Hello World!",
+  });
+
+  const changeMessage = () => {
+  // ...person to iterate all the components of person, 
+  // then change the message component to be new message
+    setPerson({ ...person, message: "Nice!" });
+  };
+  return (
+  <>
+    <h2>useState object example</h2>
+    <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+    <button className="btn" onClick={changeMessage}>
+      change me!
+    </button>
+  </>);
 };
 
 export default UseStateObject;
