37a88f4461c1dec57dd130053f7ef48b14af134c
florentianayuwono <e0851439@u.nus.edu>
2023-05-03 19:52:15 +0800

    Add basic counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+  return (
+    <>
+      <h2>useState counter example</h2>
+      <h2>Basic lols</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue((value - 1))}>dont</button>
+      <button className="btn" onClick={() => setValue((value + 1))}>yes</button>
+      <button className="btn" onClick={reset}>restart?</button>
+    </>
+  );
 };
 
 export default UseStateCounter;
