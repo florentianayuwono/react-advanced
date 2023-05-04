335efc99c6b90560b1e2ae121cb300b273e5e446
florentianayuwono <e0851439@u.nus.edu>
2023-05-04 14:30:15 +0800

    Add cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("cleanup function to remove event listener");
+      window.removeEventListener("resize@", checkSize);
+    };
+  }, []); // Add empty array will achieve similar effect of cleanup in this case only
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size}</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
